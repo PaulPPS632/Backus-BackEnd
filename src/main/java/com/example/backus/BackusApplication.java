d346da445b404abd9cc6e6ba535ce9f820fe15c2
PaulPPS632 <jeffertsap@hotmail.com>
2024-09-13 23:05:56 -0500

    first commit

---
+++ src/main/java/com/example/backus/BackusApplication.java
@@ -0,0 +1,13 @@
+package com.example.backus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackusApplication.class, args);
+	}
+
+}
